9021024dd5524b596f312bdf1ccdf6347bf3017c
Praveer Satyam <praveer.satyam@INpraveersatyam.local>
2019-05-20 16:51:18 +0530

    [Praveer] Adds read input in main class

--- src/main/java/com/poetry/PoetryApplication.java
+++ src/main/java/com/poetry/PoetryApplication.java
@@ -2,10 +2,23 @@ package com.poetry;
 
 import com.poetry.service.PoemReciter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PoetryApplication {
     public static void main(String[] args) {
+        List<String> inputArgs = new ArrayList<>();
+        for (String arg : args) {
+            inputArgs.add(arg);
+        }
         PoemReciter poemReciter = new PoemReciter();
-        String resultPoem = poemReciter.recitePoem(2);
+        int index = inputArgs.indexOf("--reveal-for-day");
+        String resultPoem = "";
+        if (inputArgs.contains("--echo")) {
+            resultPoem = poemReciter.reciteEchoPoem(Integer.parseInt(inputArgs.get(index)));
+        } else {
+            resultPoem = poemReciter.reciteEchoPoem(Integer.parseInt(inputArgs.get(index)));
+        }
         System.out.println(resultPoem);
     }
 }
